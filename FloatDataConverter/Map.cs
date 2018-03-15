f81c29b927a3a20306f4448179cf485121601408
Robby Arifandri <robby.arifandri@gmail.com>
2018-03-15 07:11:54 +0700

    Add interpolation, save to file, format as csv method.

--- FloatDataConverter/Map.cs
+++ FloatDataConverter/Map.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,28 +19,73 @@ namespace FloatDataConverter
 
         public double topLatDeg;
         public double bottomLatDeg;
-        public double rightLongDeg; 
+        public double rightLongDeg;
         public double leftLongDeg;
 
-        public Map(int numRow, int numColumn, double gridSize, double topLatDeg, double bottomLatDeg, double rightLongDeg, double leftLongDeg)
+        public Map(int numRow, int numColumn, double gridSize, double firstDataLat, double firstDataLon,
+            double topLatDeg, double bottomLatDeg, double rightLongDeg, double leftLongDeg)
         {
             this.data = new float[numRow, numColumn];
             this.numRow = numRow;
             this.numColumn = numColumn;
             this.gridSize = gridSize;
+            this.firstDataLat = firstDataLat;
+            this.firstDataLon = firstDataLon;
             this.topLatDeg = topLatDeg;
             this.bottomLatDeg = bottomLatDeg;
             this.rightLongDeg = rightLongDeg;
             this.leftLongDeg = leftLongDeg;
         }
 
+        /// <summary>
+        /// Fill in map data from path
+        /// </summary>
+        /// <param name="inputFilePath"></param>
+        public void ImportData(string inputFilePath)
+        {
+            // Input file not exists validation
+            if (!File.Exists(inputFilePath))
+            {
+                throw new ArgumentException("File not found!");
+            }
+
+            // Input file number of data not match validation
+            byte[] fileBytes = File.ReadAllBytes(inputFilePath);
+            int count = fileBytes.Length / 4;
+            if (count != (this.numRow * this.numColumn))
+            {
+                throw new ArgumentException("Number of data not match with row x column");
+            }
+
+            // Map Data
+            using (var reader = new BinaryReader(File.OpenRead(inputFilePath)))
+            {
+                for (int i = 0; i < this.numRow; i++)
+                {
+                    for (int j = 0; j < this.numColumn; j++)
+                    {
+                        this.data[i, j] = reader.ReadSingle();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns new map that represent corresponding cropped map
+        /// </summary>
+        /// <param name="croptopLatDeg"></param>
+        /// <param name="cropbottomLatDeg"></param>
+        /// <param name="croprightLongDeg"></param>
+        /// <param name="cropleftLongDeg"></param>
+        /// <returns></returns>
         public Map CropMap(double croptopLatDeg, double cropbottomLatDeg, double croprightLongDeg, double cropleftLongDeg)
         {
             var rowOffset = (int)((topLatDeg - croptopLatDeg) / gridSize);
             var rowCount = (int)((croptopLatDeg - cropbottomLatDeg) / gridSize);
-
             var columnOffset = (int)(-1 * (leftLongDeg - cropleftLongDeg) / gridSize);
             var columnCount = (int)(-1 * (cropleftLongDeg - croprightLongDeg) / gridSize);
+
+
 
             var newRow = rowCount;
             var newColumn = columnCount;
@@ -60,6 +106,10 @@ namespace FloatDataConverter
             return newMap;
         }
 
+        /// <summary>
+        /// Return string for plain text format
+        /// </summary>
+        /// <returns></returns>
         public string PrintData()
         {
             var sb = new StringBuilder();
@@ -74,19 +124,36 @@ namespace FloatDataConverter
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Return string for CSV file format
+        /// </summary>
+        /// <returns></returns>
         public string PrintDataCsvFormat()
         {
             var sb = new StringBuilder();
-            sb.AppendLine("Data Value, Latitude, Longitude");
+            sb.AppendLine(string.Format("{0}, {1}, {2}", "Data Value", "Latitude", "Longitude"));
+            var currentLat = this.firstDataLat;
+            var currentLon = this.firstDataLon;
             for (int i = 0; i < numRow; i++)
             {
                 for (int j = 0; j < numColumn; j++)
                 {
-                    sb.AppendFormat("{0,15} ", data[i, j].ToString());
+                    sb.AppendFormat("{0,15}, {1}, {2}", data[i, j].ToString(), currentLat, currentLon);
+                    currentLon += this.gridSize;
                 }
-                sb.AppendLine();
+                currentLat += this.gridSize;
             }
             return sb.ToString();
+        }
+
+        /// <summary>
+        /// Save content to file
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="contents"></param>
+        public void SaveToFile(string filePath, string contents)
+        {
+            File.WriteAllText(filePath, contents);
         }
     }
 }
--- FloatDataConverter/Program.cs
+++ FloatDataConverter/Program.cs
@@ -9,15 +9,24 @@ namespace FloatDataConverter
 {
     class Program
     {
-
+        /// <summary>
+        /// Main Program Float Data Converter
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+
+            // Init default parameters
             string inputFilePath = @"input.dat";
-            string outputFilePath = @"output.txt";
-            string outputCropFilePath = @"output_cropped.txt";
+            string outputFilePath = @"output.csv";
+            string outputCropFilePath = @"output_cropped.csv";
             int numDataRow = 480;//8; //480
             int numDataColumn = 1440;//4; //1440
             double gridSize = 0.25;//1;//0.25;
+
+            double firstDataLat = 0.05;
+            double firstDataLon = 59.95;
+
             double topLatDeg = 60;// 4;//60; //north
             double bottomLatDeg = -60;//-4;//-60; //south
             double rightLongDeg = 180; // 2;//180; //west
@@ -30,58 +39,47 @@ namespace FloatDataConverter
 
             Console.WriteLine("4-byte Float Binary Data Converter");
             Console.WriteLine("===================================");
-            Console.WriteLine("Program akan mengkonversi file 'input.dat' menjadi dua file: 'output.txt' dan 'output_cropped.txt'");
+            Console.WriteLine("Program akan mengkonversi file {0} menjadi dua file: {1} dan {2}", inputFilePath, outputFilePath, outputCropFilePath );
             Console.WriteLine("Saat ini input data diasumsikan");
-            Console.WriteLine("- Ukuran Grid: 1400 (horizontal) x 480 (vertikal)");
-            Console.WriteLine("- Resolusi Grid: 0.25");
+            Console.WriteLine("- Ukuran Grid: {0} (horizontal) x {1} (vertikal)", numDataColumn, numDataRow);
+            Console.WriteLine("- Resolusi Grid: {0}", gridSize);
             //Console.WriteLine("- Koordinat Data: 60°N - 60°S, 0°E - 180°E - 180°W - 0°W");
             Console.WriteLine("- Data dipetakan dari kiri - kanan lalu atas - bawah.");
-            Console.WriteLine("- Data pertama pada koordinat 0.05°E, 59.95°N.");
+            Console.WriteLine("- Data pertama pada koordinat 0.05°E, 59.95°N.", firstDataLon, firstDataLat);
             Console.WriteLine("- Koordinat Crop Data: 2.5°S - 5°S, 102°E - 106°E");
             Console.WriteLine(" Tekan enter untuk melanjutkan.");
             Console.ReadKey();
 
-            // Input file not exists
-            if (!File.Exists(inputFilePath))
-            {
-                throw new ArgumentException("File not found!");
-            }
-            // Read data from file
-            byte[] fileBytes = File.ReadAllBytes(inputFilePath);
-            int count = fileBytes.Length / 4;
+            // TODO: create input method
 
-            // Number of bytes not match with number of data (row x column)
-            if (count != (numDataRow * numDataColumn))
-            {
-                throw new ArgumentException("Number of data not match with row x column");
-            }
+            // Map input data and export to CSV
+            var map = new Map(
+                numDataRow, numDataColumn, gridSize, firstDataLat, firstDataLon,
+                topLatDeg, bottomLatDeg, rightLongDeg, leftLongDeg);
+            map.ImportData(inputFilePath);
+            map.SaveToFile(outputFilePath, map.PrintData());
 
-            // Mapping
-            var map = new Map(numDataRow, numDataColumn, gridSize, topLatDeg, bottomLatDeg, rightLongDeg, leftLongDeg);
-            using (var reader = new BinaryReader(File.OpenRead(inputFilePath)))
-            {
-                for (int i = 0; i < numDataRow; i++)
-                {
-                    for (int j = 0; j < numDataColumn; j++)
-                    {
-                        map.data[i, j] = reader.ReadSingle();
-                    }
-                }
-            }
-            File.WriteAllText(outputFilePath, map.PrintData());
-
-            // Interpolate Longitude. 
-            // Data menggunakan 0 BT 180 BT 180BB 0 BB sedangkan program menggunakan 180BB 0 180BT
-            // contoh: 102 (BT) direpresentasikan dlm -102 diinterpolasi menjadi -78
-            // contoh: 3 BB direpresentasikan dlm 3 diinterpolasi menjadi 177
-            var newCropLeftLongDeg = cropleftLongDeg < 0 ? -1 * (180 + cropleftLongDeg) : 180 - cropleftLongDeg;
-            var newCropRightLongDeg = croprightLongDeg < 0 ? -1 * (180 + croprightLongDeg) : 180 - croprightLongDeg;
-
+            // Crop Map and export to CSV
+            var newCropLeftLongDeg = InterpolateLon(cropleftLongDeg);
+            var newCropRightLongDeg = InterpolateLon(croprightLongDeg);
             var newMap = map.CropMap(croptopLatDeg, cropbottomLatDeg, newCropRightLongDeg, newCropLeftLongDeg);
-            File.WriteAllText(outputCropFilePath, newMap.PrintData());
+            //File.WriteAllText(outputCropFilePath, newMap.PrintData());
+            map.SaveToFile(outputCropFilePath, map.PrintDataCsvFormat());
 
             Console.WriteLine("Berhasil mengkonversi data. Cek file output.txt dan output_cropped.txt");
             Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Interpolate Longitude untuk map dengan Data menggunakan 0 BT 180 BT 180BB 0 BB 
+        /// sedangkan program menggunakan 180BB 0 180BT
+        /// contoh: 102 (BT) direpresentasikan dlm -102 diinterpolasi menjadi -78
+        /// contoh: 3 BB direpresentasikan dlm 3 diinterpolasi menjadi 177
+        /// </summary>
+        /// <returns>Longitude interpolated based on program requires</returns>
+        private static double InterpolateLon(double cropLongDeg)
+        {
+            return cropLongDeg < 0 ? -1 * (180 + cropLongDeg) : 180 - cropLongDeg;
         }
     }
 }
