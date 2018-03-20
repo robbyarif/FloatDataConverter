6c2d5ea81a42741c80e330619a4e228efbc82601
Robby Arifandri <robby.arifandri@gmail.com>
2018-03-20 21:18:51 +0700

    Add file extract.

--- FloatDataConverter/MapExtensions.cs
+++ FloatDataConverter/MapExtensions.cs
@@ -8,6 +8,84 @@ namespace FloatDataConverter
 {
     public static class MapExtensions
     {
+        public static void ConvertData()
+        {
+            // Init default parameters
+            string inputFilePath = @"input.dat";
+            string outputCsvPath = @"output.csv";
+            string outputCropCsvPath = @"output_cropped.csv";
+            int numDataRow = 480;
+            int numDataColumn = 1440;
+            double gridSize = 0.25;
+
+            // Initial upper and leftmost data: 59.95°N, 0.05°E
+            double firstDataLat = 59.95;
+            double firstDataLon = -0.05;
+
+            // South Sumatera area 2.5°S - 5°S and 102°E - 106°E
+            double croptopLatDeg = -2.5;
+            double cropbottomLatDeg = -5;
+            double croprightLongDeg = -106;
+            double cropleftLongDeg = -102;
+
+            Console.WriteLine("4-byte Float Binary Data Converter");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Program akan mengkonversi file {0} menjadi dua file: {1} dan {2}", inputFilePath, outputCsvPath, outputCropCsvPath);
+            Console.WriteLine("Saat ini input data diasumsikan");
+            Console.WriteLine("- Ukuran Grid: {0} (horizontal) x {1} (vertikal)", numDataColumn, numDataRow);
+            Console.WriteLine("- Grid Size: {0}", gridSize);
+            Console.WriteLine("- Data dipetakan dari kiri - kanan lalu atas - bawah.");
+            Console.WriteLine("- Data pertama pada koordinat {0}, {1}.", firstDataLat.ToLatString(), firstDataLon.ToLonString());
+            Console.WriteLine("- Koordinat Crop Data (Indonesia): {0} - {1}, {2} - {3}",
+                croptopLatDeg.ToLatString(), cropbottomLatDeg.ToLatString(), cropleftLongDeg.ToLonString(), croprightLongDeg.ToLonString());
+
+            string input;
+            Console.WriteLine(" Gunakan default value? (Y/N)");
+            input = Console.ReadLine();
+            if (input.Equals("n", StringComparison.OrdinalIgnoreCase))
+            {
+                Console.Write("File input: "); inputFilePath = Console.ReadLine();
+                Console.Write("File output: "); outputCsvPath = Console.ReadLine();
+                Console.Write("File cropped output: "); outputCropCsvPath = Console.ReadLine();
+                Console.Write("Ukuran Grid Horizontal: "); numDataColumn = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Ukuran Grid Vertikal: "); numDataRow = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Grid Size: "); gridSize = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Gunakan aturan di bawah untuk pengisian Latitude dan Longitude");
+                Console.WriteLine("Lat: > 0 untuk North/Utara, < 0 untuk South/Selatan");
+                Console.WriteLine("Lon: < 0 untuk East/Bujur Timur, > 0 untuk West/Bujur Barat");
+                Console.Write("Data pertama terletak pada Lat: "); firstDataLat = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Data pertama terletak pada Lon: "); firstDataLon = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Top Crop Lat: "); croptopLatDeg = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Bottom Crop Lat: "); cropbottomLatDeg = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Left Crop Lon: "); cropleftLongDeg = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Right Crop Lon: "); croprightLongDeg = Convert.ToDouble(Console.ReadLine());
+            }
+
+            try
+            {
+                // Map input data and export to CSV
+                Console.WriteLine("Importing file {0}...", inputFilePath);
+                var map = new Map(numDataRow, numDataColumn, gridSize, firstDataLat, firstDataLon.InterpolateLon());
+                Console.WriteLine("Export initial data to file {0}...", outputCsvPath);
+                map.ImportData(inputFilePath);
+                map.SaveToFile(outputCsvPath, map.PrintDataCsvFormat());
+
+                // Crop Map and export to CSV
+                Console.WriteLine("Cropping spatial data...");
+                var newMap = map.CropMap(croptopLatDeg, cropbottomLatDeg, cropleftLongDeg.InterpolateLon(), croprightLongDeg.InterpolateLon());
+                Console.WriteLine("Export cropped data to file {0}...", outputCropCsvPath);
+                map.SaveToFile(outputCropCsvPath, newMap.PrintDataCsvFormat());
+
+                Console.WriteLine("Berhasil mengkonversi data. Cek file {0} dan {1}...", outputCsvPath, outputCropCsvPath);
+                Console.ReadKey();
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine("Error: {0}", ex.Message);
+                Console.ReadKey();
+            }
+        }
+
         public static string ToLatString(this double lat)
         {
             return lat >= 0 ? string.Format("{0}°N", lat) : string.Format("{0}°S", Math.Abs(lat));
--- FloatDataConverter/Program.cs
+++ FloatDataConverter/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,86 +10,99 @@ namespace FloatDataConverter
 {
     class Program
     {
+        private static string toExtractPath = @"C:\Workspace\FloatData\to_be_extracted";
+        private static string extractedPath = @"C:\Workspace\FloatData\extracted";
+
         /// <summary>
         /// Main Program Float Data Converter
         /// </summary>
         /// <param name="args"></param>
         static void Main(string[] args)
         {
-            // Init default parameters
-            string inputFilePath = @"input.dat";
-            string outputCsvPath = @"output.csv";
-            string outputCropCsvPath = @"output_cropped.csv";
-            int numDataRow = 480;
-            int numDataColumn = 1440;
-            double gridSize = 0.25;
+            while (true)
+            {
+                // Print welcome message
+                var command = PrintWelcomeMessage();
 
-            // Initial upper and leftmost data: 59.95°N, 0.05°E
-            double firstDataLat = 59.95;
-            double firstDataLon = -0.05;
+                // Menu 1. Extract Files to Folder
+                if (command == "1")
+                {
+                    BatchExtract();
+                }
+                // Menu 2. Batch Convert Files
+                else if (command == "2")
+                {
+                    MapExtensions.ConvertData();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter correct menu number.");
+                }
 
-            // South Sumatera area 2.5°S - 5°S and 102°E - 106°E
-            double croptopLatDeg = -2.5;
-            double cropbottomLatDeg = -5;
-            double croprightLongDeg = -106;
-            double cropleftLongDeg = -102;
+            }
+        }
 
-            Console.WriteLine("4-byte Float Binary Data Converter");
+        static string PrintWelcomeMessage()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Data Converter Program");
             Console.WriteLine("===================================");
-            Console.WriteLine("Program akan mengkonversi file {0} menjadi dua file: {1} dan {2}", inputFilePath, outputCsvPath, outputCropCsvPath);
-            Console.WriteLine("Saat ini input data diasumsikan");
-            Console.WriteLine("- Ukuran Grid: {0} (horizontal) x {1} (vertikal)", numDataColumn, numDataRow);
-            Console.WriteLine("- Grid Size: {0}", gridSize);
-            Console.WriteLine("- Data dipetakan dari kiri - kanan lalu atas - bawah.");
-            Console.WriteLine("- Data pertama pada koordinat {0}, {1}.", firstDataLat.ToLatString(), firstDataLon.ToLonString());
-            Console.WriteLine("- Koordinat Crop Data (Indonesia): {0} - {1}, {2} - {3}",
-                croptopLatDeg.ToLatString(), cropbottomLatDeg.ToLatString(), cropleftLongDeg.ToLonString(), croprightLongDeg.ToLonString());
+            Console.WriteLine("1. Batch Extract .dat.gz Files");
+            Console.WriteLine("2. Batch Convert .dat Files");
+            Console.WriteLine("Pilih menu:");
+            return Console.ReadLine();
+        }
 
-            // TODO: create input method
-            string input;
-            Console.WriteLine(" Gunakan default value? (Y/N)");
-            input = Console.ReadLine();
-            if (input.Equals("n", StringComparison.OrdinalIgnoreCase))
+        static void BatchExtract()
+        {
+            Console.WriteLine("1. Batch Extract .dat.gz Files");
+            Console.WriteLine("===================================");
+            Console.WriteLine("Instruksi:");
+            Console.WriteLine("1. Siapkan folder bernama {0} yang berisi file-file .dat.gz.", toExtractPath);
+            Console.WriteLine("2. Program akan mengekstrak file-file ke folder {0}.", extractedPath);
+            Console.WriteLine("Tekan enter untuk memulai!"); Console.ReadKey();
+            ExtractFiles();
+        }
+
+        static void ExtractFiles()
+        {
+            // Check input folder exists
+            if (!Directory.Exists(toExtractPath))
             {
-                Console.Write("File input: "); inputFilePath = Console.ReadLine();
-                Console.Write("File output: "); outputCsvPath = Console.ReadLine();
-                Console.Write("File cropped output: "); outputCropCsvPath= Console.ReadLine();
-                Console.Write("Ukuran Grid Horizontal: "); numDataColumn = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Ukuran Grid Vertikal: "); numDataRow = Convert.ToInt32(Console.ReadLine());
-                Console.Write("Grid Size: "); gridSize = Convert.ToDouble(Console.ReadLine());
-                Console.WriteLine("Gunakan aturan di bawah untuk pengisian Latitude dan Longitude");
-                Console.WriteLine("Lat: > 0 untuk North/Utara, < 0 untuk South/Selatan");
-                Console.WriteLine("Lon: < 0 untuk East/Bujur Timur, > 0 untuk West/Bujur Barat");
-                Console.Write("Data pertama terletak pada Lat: "); firstDataLat = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Data pertama terletak pada Lon: "); firstDataLon = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Top Crop Lat: "); croptopLatDeg = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Bottom Crop Lat: "); cropbottomLatDeg = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Left Crop Lon: "); cropleftLongDeg = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Right Crop Lon: "); croprightLongDeg = Convert.ToDouble(Console.ReadLine());
+                throw new DirectoryNotFoundException("");
             }
 
-            try
+            // Delete output folder if exists
+            if (Directory.Exists(extractedPath))
             {
-                // Map input data and export to CSV
-                Console.WriteLine("Importing file {0}...", inputFilePath);
-                var map = new Map(numDataRow, numDataColumn, gridSize, firstDataLat, firstDataLon.InterpolateLon());
-                Console.WriteLine("Export initial data to file {0}...", outputCsvPath);
-                map.ImportData(inputFilePath);
-                map.SaveToFile(outputCsvPath, map.PrintDataCsvFormat());
-
-                // Crop Map and export to CSV
-                Console.WriteLine("Cropping spatial data...");
-                var newMap = map.CropMap(croptopLatDeg, cropbottomLatDeg, cropleftLongDeg.InterpolateLon(), croprightLongDeg.InterpolateLon());
-                Console.WriteLine("Export cropped data to file {0}...", outputCropCsvPath);
-                map.SaveToFile(outputCropCsvPath, newMap.PrintDataCsvFormat());
-
-                Console.WriteLine("Berhasil mengkonversi data. Cek file {0} dan {1}...", outputCsvPath, outputCropCsvPath);
-                Console.ReadKey();
+                Directory.Delete(extractedPath);
             }
-            catch (ArgumentException ex)
+            Directory.CreateDirectory(extractedPath);
+
+            // Decompress
+            DirectoryInfo directorySelected = new DirectoryInfo(toExtractPath);
+            foreach (FileInfo fileToDecompress in directorySelected.GetFiles("*.gz"))
             {
-                Console.WriteLine("Error: {0}", ex.Message);
-                Console.ReadKey();
+                Decompress(fileToDecompress, new DirectoryInfo(extractedPath));
+            }
+        }
+
+        static void Decompress(FileInfo fileToDecompress, DirectoryInfo outputPath)
+        {
+            using (FileStream originalFileStream = fileToDecompress.OpenRead())
+            {
+                string currentFileName = fileToDecompress.Name;
+                string newFileName = currentFileName.Remove(currentFileName.Length - fileToDecompress.Extension.Length);
+
+                using (FileStream decompressedFileStream = File.Create(outputPath.FullName + "\\" + newFileName))
+                {
+                    using (GZipStream decompressionStream = new GZipStream(originalFileStream, CompressionMode.Decompress))
+                    {
+                        decompressionStream.CopyTo(decompressedFileStream);
+                        Console.WriteLine("Decompressed: {0}", fileToDecompress.Name);
+                    }
+                }
             }
         }
     }
